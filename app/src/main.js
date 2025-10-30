d1e696a841a112658c67bccf37defc431cde6f95
shinyglasses <crunchytvtable@gmail.com>
2025-10-30 12:32:25 -0400

    assets. some js

--- app/src/main.js
+++ app/src/main.js
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 import {createSaveFile, accessSaveFile} from './save_files.js'
 import { createPet } from './pet.js'
+import { changeTheme } from './misc.js'
 document.querySelector('#app').innerHTML = `
  
 `
@@ -11,4 +12,3 @@ pet = createPet();
 setupCounter(document.querySelector('#counter'));
 createSaveFile('me', pet);
 accessSaveFile();
-
--- app/src/misc.js
+++ app/src/misc.js
@@ -1,13 +1,28 @@
-btn = document.querySelector('.theme')
-container = document.querySelector('.container')
-btn.addEventListener('click', function() {
-    if (container.classList.contains('light')) {
-        container.classList.remove('light');
-        container.classList.add('dark');
-    } else if (container.classList.contains('dark')) {
-        container.classList.remove('dark');
-        container.classList.add('light')
-    }
-    }
-)
+function changeTheme() { 
+    btn = document.querySelector('.theme');
+    container = document.body;
+    btn.addEventListener('click', function() {
+        if (container.classList.contains('light')) {
+            container.classList.remove('light');
+            container.classList.add('dark');
+        } else if (container.classList.contains('dark')) {
+            container.classList.remove('dark');
+            container.classList.add('light')
+        }
+        }
+    )
+}
 
+export function getPetImage(petStatus) {
+    //if i add pets besides dogs i need to change 
+    // the status_dog 
+    const array = {
+        'tired': 'public/tired_dog.png',
+        'joyful': 'public/joyful_dog.png',
+        'sad': 'public/sad_dog.png',
+        'happy': 'public/happy_dog.png'
+    }
+    const imgName = array[petStatus];
+    return imgName
+}
+changeTheme()
